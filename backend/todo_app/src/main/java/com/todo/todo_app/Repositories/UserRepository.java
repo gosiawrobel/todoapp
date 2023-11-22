c99ef18aaca65bce0e06542977df2fa103450e34
gosiawrobel <mwrobel.prog@gmail.com>
2023-11-22 09:28:26 +0100

    Code clean up on backend

--- backend/todo_app/src/main/java/com/todo/todo_app/Repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.todo.todo_app.Repositories;
-
-import com.todo.todo_app.Entities.UserTodo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserTodo, Long> {
-}
